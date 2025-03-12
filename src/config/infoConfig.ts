fa50941b6bab87eeb7bf9df4cb4e482546d8362f
Aurora-yang-git <202760123@stu.scls-sh.org>
2025-03-12 13:48:26 +0800

    update award

--- src/config/infoConfig.ts
+++ src/config/infoConfig.ts
@@ -11,11 +11,11 @@ export const email = '202760123@stu.scls-sh.org'
 export const githubUsername = 'Aurora-yang-git'
 
 // about page
-export const aboutMeHeadline = 'Who Are You and Why Should I Care?'
+export const aboutMeHeadline = 'Who Am I and Why Should You Care?'
 export const aboutParagraphs = [
-  "Coucou Les Amis! My name is Aurora Yang. I'm from Hubei and an alumnus of BUPT, UChicago, and Tsinghua. I’ve been teaching Computer Science at SCLS since 2020.",
-  'Currently I am working on a new course called AI Camp, an innovative course where high school students create real-world products and startups while exploring the frontiers of AI.',
-  "I started this blog to share the insights I learn every day. Most blogs focus on education in Artificial Intelligence and general computer science, while others share the life lessons I've learned.",
+  "Hello! My name is Aurora Yang. I'm from Shanghai and currently a first-year high school student. I have a strong interest in physics, computer science, and artificial intelligence, especially in exploring the principles behind how the world works and using that knowledge to benefit humanity.",
+  "Currently, I'm working on a math modeling paper and an AI-related project now. I'm also passionate about learning, especially science. I have read many books related to sociology, psychology, science fiction, and science.",
+  "I started this blog to share the insights I gain from my studies and projects. Most of my posts focus on science, AI, and education, but I also enjoy reflecting on life lessons and personal growth."
 ]
 
 // blog
